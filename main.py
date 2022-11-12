53f652290868d037c3b2bccd291567604fbcdcdc
AAAxis <polskoydm@gmail.com>
2022-11-12 16:21:25 +0200

    ver 2

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 
-from flask import Flask, render_template, request, redirect, send_from_directory
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgresql@localhost:5433/flasksql'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'key'
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(120), default='image.jpg')
+    password = db.Column(db.String(100), nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
